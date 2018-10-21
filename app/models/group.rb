5b13f03f377ac545f2720e6b119acfb78688afb1
Santosh <santosh.poudyal@gmail.com>
2018-10-22 09:47:09 +1100

    added data and model scope to model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
-    #has_one: groupuser
+    has_many :group_users
+    has_many :users, :through => :group_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-    #has_many: groupusers
+    has_many :group_users
+    has_many :groups, :through => :group_users
+
+    def self.by_name_and_number(name:, number:)
+        self.where(name:"#{name}#{number}")
+
+    end
 end
